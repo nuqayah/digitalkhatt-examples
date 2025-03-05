1c5f410bd92592708901ef9cc6729d6b6aee65d5
Mustafa J <mustafa.0x@gmail.com>
2025-03-05 20:53:35 +0300

    build: minor

--- build.js
+++ build.js
@@ -11,15 +11,8 @@ const cssnano_conf = {preset: ['default', {normalizeUrl: false, discardComments:
 const min_css = async s => (await postcss([pseudo_is, autoprefixer, cssnano(cssnano_conf)]).process(s, {from: undefined})).css
 const apply_repls = (s, repls) => repls.reduce((a, b) => a[b[2] ? 'replaceAll' : 'replace'](b[0], b[1]), s)
 
-const polyfills = ['Object.fromEntries', 'Intl.RelativeTimeFormat.~locale.ar']
-const pf_url = `document.write('<script src="https://polyfill.io/v3/polyfill.min.js?features=${polyfills.join(',')}"><\\/script>')`
-const pf_script = `((${polyfills.map(pf => pf.replace(/\.~.*/, '')).join(' && ')}) || ${pf_url})`
 
-// Service worker
-fs.writeFileSync('dist/sw.js', await min_js(r('src/util/sw.js').replaceAll('$TS', Date.now())))
-
-// Polyfills
-const prod_script = await min_js(r('src/util/prod.js').replace('UA-KEY', 'UA-127500665-'))
+const prod_script = ''
 
 // Gather assets
 const [css, js] = await Promise.all([
@@ -32,8 +25,8 @@ const [css, js] = await Promise.all([
 // Combine
 const pg = apply_repls(r('index.html'), [
     [/>\n+ */g, '>'],
-    [/<!-- styles -->/, () => `<style>${css}</style>`],
-    [/<script src[\s\S]*<\/script>/, () => `<script>${pf_script};${prod_script}</script><script type=module>${js}</script>`],
+    [/(?<=<\/title>)/, () => `<style>${css}</style>`],
+    [/<script src[\s\S]*<\/script>/, () => `<script>${prod_script}</script><script type=module>${js}</script>`],
 ])
 const icon_repls = [[/ xmlns=".*?"/, ''], [/id="/g, 'id="icon-'], [/\n */g, ''], [/(\d")\//g, '$1 /'], [/="([^ ]+)"/g, '=$1']]
 fs.writeFileSync('dist/index.html', pg + apply_repls(r('public/icons.svg'), icon_repls))
